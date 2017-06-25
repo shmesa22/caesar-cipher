2f8a158affa3b75681864f2f8b1597f9247fc29b
shmesa22 <shmesa23@gmail.com>
2017-06-25 10:04:37 -0500

    Implement cipher text functionality

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$('#cipher').on('click', function() {
+  let lettersLow = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  let lettersUpp = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  let toCipher = $(this).prev().children().val();
+  let key = 3;
+  let arr = toCipher.split('');
+  let cipheredLetters = [];
+
+  for (let i = 0; i < toCipher.length; i++) {
+    let lowercase = lettersLow.indexOf(arr[i]);
+    let uppercase = lettersUpp.indexOf(arr[i]);
+
+     if (lowercase === -1 && uppercase === -1) {
+      cipheredLetters.push(arr[i]);
+     } else if (lowercase >= 0) {
+      let dif = lowercase - key;
+      if (dif >= 0) {
+        cipheredLetters.push(lettersLow[dif]);
+      } else {
+        dif += 26;
+        cipheredLetters.push(lettersLow[dif]);
+      }
+    } else if (uppercase >= 0){
+      let dif = uppercase - key;
+      if (dif >= 0) {
+        cipheredLetters.push(lettersUpp[dif]);
+      } else {
+        dif += 26;
+        cipheredLetters.push(lettersUpp[dif]);
+      }
+    }
+  }
+
+  toCipher = cipheredLetters.join('');
+  $(this).parent().next().children().children().val(toCipher);
+});
+
